e2c746e635ce3f109c1c4f9b6c715563f79993d6
Adam W. Dace <colonelforbin74@gmail.com>
2014-11-19 20:26:25 -0600

    Minor Minecraft changes.

--- devel/maze/minecraft/minecraft.sh
+++ devel/maze/minecraft/minecraft.sh
@@ -9,7 +9,8 @@
 
 echo "INFO: Starting Minecraft v1.8 server..."
 # Launch java.
-java -Xmx512M -Xms512M -jar minecraft_server.jar nogui < /dev/null >> minecraft.out 2>&1 &
+java -Xmx768M -Xms768M -jar minecraft_server.jar nogui < /dev/null >> minecraft.out 2>&1 &
+#nice -n 5 java -Xmx512M -Xms512M -jar minecraft_server.jar nogui < /dev/null >> minecraft.out 2>&1 &
 # Write PID file.
 sleep 1
 pidof java > minecraft.pid
