c98873dbf758314455e20f57a8eafb4f96f3c425
Adam W. Dace <colonelforbin74@gmail.com>
2014-12-02 22:47:50 -0600

    Starting to move bits around.

---
+++ src/com/github/stockanalyst/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * $Id$
+ *
+ * (c) Copyright 2013 Adam W. Dace.  All Rights Reserved.
+ * Please see the accompanying COPYING file for license information.
+ */
+
+package com.github.stockanalyst;
+
+class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!");
+    }
+}
