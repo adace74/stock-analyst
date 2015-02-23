44d11014fc0829cd50fcabe2ae28fa43c7977ecd
Adam W. Dace <colonelforbin74@gmail.com>
2015-02-22 19:22:28 -0500

    More fine-tuning of some parameters.

--- devel/maze/startup.sh
+++ devel/maze/startup.sh
@@ -9,11 +9,11 @@
 
 echo "INFO: Activating custom resolv.conf..."
 sudo cp /etc/resolv.conf.custom /etc/resolv.conf
-#echo "INFO: Starting vnstatd..."
-#sudo /usr/sbin/vnstatd -d			# Start vnstatd
-#echo "INFO: Starting Apache HTTPd..."
-#sudo /usr/local/apache2/bin/apachectl start	# Start HTTPd
-#echo "INFO: Starting Apache Traffic Server..."
-#sudo /usr/local/bin/trafficserver start		# Start ATS
+echo "INFO: Starting vnstatd..."
+sudo /usr/sbin/vnstatd -d			# Start vnstatd
+echo "INFO: Starting Apache HTTPd..."
+sudo /usr/local/apache2/bin/apachectl start	# Start HTTPd
+echo "INFO: Starting Apache Traffic Server..."
+sudo /usr/local/bin/trafficserver start		# Start ATS
 
 echo "INFO: Done."
