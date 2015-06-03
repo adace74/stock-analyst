7cb69a0b65a15ed2d51e2c63745b6083cba7fc30
Adam W. Dace <colonelforbin74@gmail.com>
2015-06-03 01:07:58 -0400

    Adding new file.

---
+++ devel/antelope/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+######################################################################
+#
+# $Id$
+#
+# Description: Quick-hack startup script for FreeBSD.
+#
+######################################################################
+
+echo "INFO: Activating custom swapfile..."
+sudo mdconfig -a -t vnode -f /usr/swap0 -u 0
+sudo swapon /dev/md0
+echo "INFO: Starting vnstatd..."
+sudo /usr/local/sbin/vnstatd -d			# Start vnstatd
+echo "INFO: Starting Apache Traffic Server..."
+sudo /usr/local/bin/trafficserver start		# Start ATS
+
+echo "INFO: Done."
