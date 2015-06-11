547559c0d0b1541e5dd2596a4f3ec69784d2dd50
Adam W. Dace <colonelforbin74@gmail.com>
2015-06-11 18:42:16 -0400

    Various small changes.

--- devel/antelope/startup.sh
+++ devel/antelope/startup.sh
@@ -8,7 +8,7 @@
 ######################################################################
 
 echo "INFO: Activating custom swapfile..."
-sudo mdconfig -a -t vnode -f /usr/swap0 -u 0
+sudo mdconfig -a -t vnode -f /swapfile -u 0
 sudo swapon /dev/md0
 echo "INFO: Starting vnstatd..."
 sudo /usr/local/sbin/vnstatd -d			# Start vnstatd
