3d222283196f74bb052c1fc7a089b2a1e9c0ff77
Adam W. Dace <colonelforbin74@gmail.com>
2015-06-12 21:26:04 -0400

    Switching servers once more, minor cleanups.

--- devel/antelope/startup.sh
+++ devel/antelope/startup.sh
@@ -3,15 +3,10 @@
 #
 # $Id$
 #
-# Description: Quick-hack startup script for FreeBSD.
+# Description: Quick-hack startup script for Linux.
 #
 ######################################################################
 
-echo "INFO: Activating custom swapfile..."
-sudo mdconfig -a -t vnode -f /swapfile -u 0
-sudo swapon /dev/md0
-echo "INFO: Starting vnstatd..."
-sudo /usr/local/sbin/vnstatd -d			# Start vnstatd
 echo "INFO: Starting Apache Traffic Server..."
 sudo /usr/local/bin/trafficserver start		# Start ATS
 
