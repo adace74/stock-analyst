6a66ff4269480ee49258247fc99216cf735d601d
Adam W. Dace <colonelforbin74@gmail.com>
2015-06-16 05:49:33 -0400

    Various minor changes.

--- devel/linux/firewall_setup.sh
+++ devel/linux/firewall_setup.sh
@@ -9,10 +9,10 @@
 
 echo "INFO: Setting up firewall rules..."
 # Apache HTTP Server
-#sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
 # Apache TrafficServer
 sudo firewall-cmd --zone=public --add-port=8080/tcp --permanent
 # Minecraft
 #sudo firewall-cmd --zone=public --add-port=25565/tcp --permanent
-#sudo firewall-cmd --reload
+sudo firewall-cmd --reload
 echo "INFO: Done."
